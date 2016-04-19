639f85eba88d0e33a9ca04cf6f28158236491f3e
Simon Urli <simon.urli@gmail.com>
2016-04-19 17:00:43 +0200

    SERVICES-138 Implement method to push datas

--- scripts/routers/CRUDRouter.ts
+++ scripts/routers/CRUDRouter.ts
@@ -2,6 +2,7 @@
  * @author Simon Urli <simon@pulsetotem.fr>
  */
 
+/// <reference path="../core/DatabaseConnection.ts" />
 
 class CRUDRouter extends RouterItf {
 
@@ -16,6 +17,28 @@ class CRUDRouter extends RouterItf {
     }
 
     createData(req : any, res : any) {
+        var connection = DatabaseConnection.getConnection();
 
+        if (connection == null) {
+            Logger.error("The service is not connected to a DB.");
+            res.status(500).send({'error': 'DB not connected.'});
+        } else {
+            var stat = req.body;
+
+            var collection = stat.collection;
+            delete stat.collection;
+
+            connection.collection(collection).insertOne(stat, function(err, result) {
+                if (err != null) {
+                    Logger.error("Error while inserting data in collection "+collection);
+                    Logger.error(err);
+
+                    res.status(500).send({'error': 'Error while inserting data in collection '+collection});
+                } else {
+                    Logger.debug("Inserting data "+result.insertedId._id+" in collection "+collection);
+                    res.status(200).send();
+                }
+            });
+        }
     }
 }
